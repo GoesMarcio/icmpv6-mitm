3ea2e42913d6ac0afdc97b17d486a75104ff1aec
pedro <pedrochem@hotmail.com>
2021-07-08 02:42:53 -0300

    receiving icmpv6 packets

--- mitmv2.py
+++ mitmv2.py
@@ -9,30 +9,42 @@ ETH_LENGTH = 14
 def bytes_to_mac(bytesmac):
     return ":".join("{:02x}".format(x) for x in bytesmac)
 
-def send_icmpv6_advertisement():
+def send_icmpv6_advertisement(ip_src):
     pass
 
 def receive_ipv6(packet): 
     first_word, payload_legth, next_header, hoplimit = struct.unpack('>IHBB', packet[0:8])
+    sender_ip = packet[8:24]
     payload = packet[40:]
 
     if next_header == socket.IPPROTO_ICMPV6:
         icmp_type, icmp_code, icmp_chekcsum = struct.unpack('>BBH', payload[:4])
 
         if icmp_type == 133:
-            print("Router Solicitation")
+            print("\n-------------------------------------------------------------")
+            print("Router Solicitation: ")
+            print("Sender IP: ",sender_ip)
+            print("-------------------------------------------------------------\n")
+            send_icmpv6_advertisement(sender_ip)
 
+            
 
 if __name__ == '__main__':
+    ''' Next steps:
+        1) avoid atacking ourselves (verify if the sender's ip == our ip)
+        2) keep track of compromised machines to print instead of attacking again (add compromised machines ip to a list)
+        3) implement 'send_router_advertisement'
+        4) check if line 17 works (maybe necessary to change some types)
+    '''
     try:
         s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(ETH_P_ALL))
     except OSError as msg:
         print('Error'+str(msg))
         sys.exit(1)
 
-    print('Socket created!')
+    print('Starting...')
 
-    s.bind(('enp0s3',0))
+    s.bind(('eth0',0))
 
     while True:
         packet, addr = s.recvfrom(65536)
@@ -42,6 +54,6 @@ if __name__ == '__main__':
 
         # checks for ipv6 packets
         if eth[2] == IPV6:
-            print("here")
-            mac_src = bytes_to_mac(eth[1])
+            print("IPV6 packet received, addr:",addr)
+            #mac_src = bytes_to_mac(eth[1])
             receive_ipv6(packet[ETH_LENGTH:])
